e365b06806e21ab0e7f9d0ae36678ab504078e6e
alexrren <alexrren@tencent.com>
2025-05-29 21:10:14 +0800

    feat: 新增模板类 my_vector

--- main.cpp
+++ main.cpp
@@ -3,13 +3,31 @@
 #include "my_operator.h"
 using namespace std;
 
-int main() {
+template<class T>
+class my_vector {
+public:
+    typedef T value_type;
+    typedef T* iterator;//迭代器指针
+    typedef T& reference;//引用
 
+    my_vector(int len=0) :m_len(len),m_Data(nullptr),start(nullptr),pos(0) {
+
+    }
+protected:
+    iterator m_Data;//指向数据的指针
+    iterator start;//起始地址
+    int m_len;//数据长度
+    int pos;//当前位置
+};
+int main() {
+    /*
+    my_operator测试
     Complex a(1, 2), b(3, 4);
     a += b;
     a.print();
     a *= b;
     a.print();
+    */
     /*
     my_template测试
     int a=1, b=2;

602296f08c5241bf4582e83ed0f5bac834f11d13
alexrren <alexrren@tencent.com>
2025-05-30 16:20:02 +0800

    feat: 实现并优化 vector 类及其相关功能

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <vector>
 #include "my_template.h"
 #include "my_operator.h"
 using namespace std;
@@ -6,13 +7,66 @@ using namespace std;
 template<class T>
 class my_vector {
 public:
-    typedef T value_type;
+    typedef T value_type;//类型
     typedef T* iterator;//迭代器指针
     typedef T& reference;//引用
 
-    my_vector(int len=0) :m_len(len),m_Data(nullptr),start(nullptr),pos(0) {
+    my_vector(int len=0) :m_Data(nullptr),start(nullptr),m_len(len),pos(0) {
+        if (len > 0) {
+            //创建数组
+            m_Data = new T[len];
+            start = m_Data;
+        }
+    }
 
+    ~my_vector() {
+        if (m_Data) {
+            delete[] m_Data;
+            m_Data = nullptr;
+        }
+    }
 
+    void push_back(const T& data) {
+
+        if(pos==m_len) {//扩容
+            int new_len = m_len * 2;
+            T* new_data = new T[new_len];
+            for (int i = 0; i < m_len; i++) {
+                new_data[i] = m_Data[i];
+            }
+            delete[] m_Data;
+            m_Data = new_data;
+            m_len = new_len;
+        }
+        m_Data[pos++] = data;
+    }
+
+    value_type pop_back() {
+        return m_Data[--pos];
+    }
+
+    int size() {
+        return pos;
+    }
+
+    int capacity() {
+        return m_len;
+    }
+    iterator begin() {
+        return start;
+    }
+
+    iterator end() {
+        return start + pos;
+    }
+
+    reference operator[](int index) {
+        if(index<=pos)
+            return m_Data[index];
+        else {
+            printf("越界");
+            return 0;
+        }
     }
 protected:
     iterator m_Data;//指向数据的指针
@@ -21,6 +75,21 @@ protected:
     int pos;//当前位置
 };
 int main() {
+    my_vector<int> vec(10);
+    for (int i = 0; i < vec.capacity(); i++) {
+        vec.push_back(i);
+    }
+    for(my_vector<int>::iterator it=vec.begin(); it!=vec.end();it++) {
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+    for(int i:vec) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    for(int i=0; i<vec.size(); i++) {
+        cout<<vec[i]<<" ";
+    }
     /*
     my_operator测试
     Complex a(1, 2), b(3, 4);
